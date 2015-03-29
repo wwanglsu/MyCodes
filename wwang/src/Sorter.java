61472dd5a491e4c9738e6e498de18084b38bdd2a
wwang <wwanglsu@gmail.com>
2015-03-29 00:17:16 -0700

    add Sorter.java

---
+++ wwang/src/Sorter.java
@@ -0,0 +1,16 @@
+import java.util.Arrays;
+
+import wang.utility.ArrayUtility;
+
+
+public class Sorter {
+
+	public static void main(String[] args){
+		int[] array = ArrayUtility.getInstance().generateRandomArray(20, 30);
+
+		System.out.println(Arrays.toString(array));
+
+
+	}
+
+}
