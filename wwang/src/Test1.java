08b9a5ea5c88f1303c1ad33000dacd449b9909c2
wwang <wwanglsu@gmail.com>
2015-03-31 00:34:22 -0700

    add HashSet class and test class

---
+++ wwang/src/Test1.java
@@ -0,0 +1,20 @@
+import junit.framework.Assert;
+
+import org.junit.Test;
+
+
+public class Test1 {
+
+
+	@Test
+	public void test() {
+		//fail("Not yet implemented");
+		Assert.assertEquals(true, true);
+	}
+
+	@Test
+	public void test2() {
+		//fail("Not yet implemented");
+		Assert.assertEquals(false, true);
+	}
+}
