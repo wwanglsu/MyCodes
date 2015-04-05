df281db5f5a049cb36c73c5fbe9ab0fd4f377a5c
wwang <wwanglsu@gmail.com>
2015-04-04 21:53:37 -0700

    chapter 1 questions

--- wwang/src/Test1.java
+++ wwang/src/Test1.java
@@ -1,5 +1,3 @@
-import junit.framework.Assert;
-
 import org.junit.Test;
 
 
@@ -8,8 +6,17 @@ public class Test1 {
 
 	@Test
 	public void test() {
-		//fail("Not yet implemented");
-		Assert.assertEquals(true, true);
+		String str1="abcdefbghij";
+		String str2="abcdefghij";
+		char c1=str1.charAt(0);
+		for(int i=1; i<str1.length(); i++){
+			c1=(char)(c1^str1.charAt(i));
+		}
+		char c2=str2.charAt(0);
+		for(int i=1; i<str2.length(); i++){
+			c2=(char)(c2^str2.charAt(i));
+		}
+		System.out.println(  (char)(c1^c2) );
 	}
 
 	@Test
