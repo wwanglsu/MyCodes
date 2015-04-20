6765878969f17d31e3f8770ae02b36987a44b3a5
wwang <wwanglsu@gmail.com>
2015-04-19 22:45:59 -0700

    wang.c9

--- wwang/src/wang/c9/Questions.java
+++ wwang/src/wang/c9/Questions.java
@@ -1,7 +1,11 @@
 package wang.c9;
 
+import java.awt.Color;
 import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.HashSet;
 import java.util.Hashtable;
+import java.util.Set;
 
 import wang.c7.Point;
 import wang.utility.ArrayUtility;
@@ -17,12 +21,20 @@ public class Questions {
         maze=ArrayUtility.getInstance().randomMatrix(10, 10, 0, 5);
         ArrayList<Point> path = new ArrayList<Point>();
         Hashtable<Point, Boolean> cache = new Hashtable<Point, Boolean>();
-        boolean success = getPath(7, 3, path, cache);
+        boolean success = getPath(9, 9, path, cache);
         System.out.println("path: "+path.size());
         int[] array={-10,-5,2,2,2,3,4,7,9,12,13};
         System.out.println("find magic index: "+findMagicIndex(array));
         System.out.println("find magic index: "+findMagicIndex_Recurse(array, 0, array.length-1));
         System.out.println("find magic index: "+findMagicIndex_DP(array, 0, array.length-1));
+
+        System.out.println("all sets: "+getSubsets(new ArrayList<Integer>(Arrays.asList(1,2,3)), 0) );
+        System.out.println("all sets: "+getSubsets_Iterative(new ArrayList<Integer>(Arrays.asList(1,2,3))) );
+
+        System.out.println("string permutation: "+getAllPermutation("abc"));
+        System.out.println("all parentheses: "+getAllParentheses(3) );
+
+        System.out.println(makeChange(61));
     }
     /****9.1*runing stair with n steps********/
     static int runStair_Recursive(int num){
@@ -140,5 +152,166 @@ public class Questions {
     }
     /****9.3 magic index***********/
 
+    /*****9.4 get all subset of a set*************************/
+    static ArrayList<ArrayList<Integer>> getSubsets(ArrayList<Integer> set, int index){
+        ArrayList<ArrayList<Integer>> allsubsets;
+        if(set.size()==index){
+            allsubsets=new ArrayList<ArrayList<Integer>>();
+            allsubsets.add(new ArrayList<Integer>());
+        }else{
+            allsubsets=getSubsets(set, index+1);
+            int item=set.get(index);
+            ArrayList<ArrayList<Integer>> more=new ArrayList<ArrayList<Integer>>();
+            for(ArrayList<Integer> subset: allsubsets){
+                ArrayList<Integer> newsubset=new ArrayList<Integer>();
+                newsubset.addAll(subset);
+                newsubset.add(item);
+                more.add(newsubset);
+            }
+            allsubsets.addAll(more);
+        }
+        return allsubsets;
+    }
 
+    static ArrayList<ArrayList<Integer>> getSubsets_Iterative(ArrayList<Integer> set){
+        ArrayList<ArrayList<Integer>> allsubset=new ArrayList<ArrayList<Integer>>();
+        int num=1<< set.size();
+        for(int i=0; i<num; i++){
+            ArrayList<Integer> subset=convertIntToSet(i, set);
+            allsubset.add(subset);
+        }
+        return allsubset;
+    }
+    private static ArrayList<Integer> convertIntToSet(int k, ArrayList<Integer> set){
+        ArrayList<Integer> subset=new ArrayList<Integer>();
+        int index=0;
+        for(int i=k; i>0; i=i/2){
+            if(i % 2 !=0){
+                subset.add(set.get(index));
+            }
+            index++;
+        }
+        return subset;
+    }
+    /*****9.4 get all subset of a set*************************/
+
+    /*****9.5 computer all permutations of a string***********/
+    static ArrayList<String> getAllPermutation(String str){
+        if(str==null) {
+            return null;
+        }
+        ArrayList<String> permutations=new ArrayList<String>();
+        if(str.isEmpty()){
+            permutations.add("");
+            return permutations;
+        }
+        char first=str.charAt(0);
+        String remainder=str.substring(1);
+        ArrayList<String> words=getAllPermutation(remainder);
+        for(String word: words){
+            for(int i=0; i<=word.length(); i++){
+                String s=insertCharAt(word, first, i);
+                permutations.add(s);                
+            }
+        }
+        return permutations;
+    }
+    private static String insertCharAt(String word, char c, int position){
+        String left=word.substring(0, position);
+        String right=word.substring(position);
+        return left+c+right;
+    }
+    /*****9.5 computer all permutations of a string***********/
+
+    /*****9.6 all valid parentheses*************************/
+    static Set<String> getAllParentheses(int num){
+        Set<String> set=new HashSet<String>();
+        if(num==0) {
+            set.add("");
+        } else{
+            Set<String> pre=getAllParentheses(num-1);
+            for(String str:pre){
+                for(int i=0; i<str.length(); i++){
+                    if(str.charAt(i)=='('){
+                        String s=insertInside(str, i);
+                        set.add(s);
+                    }
+                }
+                if(!set.contains("()"+str)){
+                    set.add("()"+str);
+                }
+            }
+        }
+        return set;
+    }
+    private static String insertInside(String str, int leftIndex){
+        String left=str.substring(0, leftIndex+1);
+        String right=str.substring(leftIndex+1);
+        return left+"()"+right;
+    }
+    /*****9.6 all valid parentheses*************************/
+
+    /******9.7 paint fill***********************/
+    static boolean paintFill(Color[][] screen, int x, int y, Color newColor){
+        return paintFill(screen, x, y, screen[y][x], newColor);
+    }
+    private static boolean paintFill(Color[][] screen, int x, int y, Color oColor, Color nColor){
+        if(x<0 || x>=screen[0].length || y<0 || y>=screen.length) {
+            return false;
+        }
+        if(screen[y][x]==oColor){
+            screen[y][x]=nColor;
+            paintFill(screen, x-1, y, oColor, nColor);
+            paintFill(screen, x+1, y, oColor, nColor);
+            paintFill(screen, x, y-1, oColor, nColor);
+            paintFill(screen, x, y+1, oColor, nColor);
+        }
+        return true;
+    }
+    /******9.7 paint fill***********************/
+
+    /******9.8 represent n cents************************/
+    static int quarters=2, dimes=4, nickles=5, pennies=7;
+    static int makeChange(int cents){
+        int[] denominations={25,10,5,1};
+        int x=makeChange(cents, 25);
+        int y=makeChange(cents, denominations, 0);
+        if(x!=y) {
+            System.out.println("Error");
+        }
+        return x;
+    }
+
+    static int makeChange(int cents, int denomination){
+        int next_denom=0;
+        switch (denomination) {
+            case 25 :
+                next_denom=10;
+                break;
+            case 10:
+                next_denom=5;
+                break;
+            case 5:
+                next_denom=1;
+                break;
+            case 1:
+                return 1;
+        }
+        int way=0;
+        for(int i=0; i*denomination <= cents; i++){
+            way=way+makeChange(cents-i*denomination, next_denom);
+        }
+        return way;
+    }
+    static int makeChange(int cents, int[] denoms, int choice){
+        if(choice +1 >= denoms.length) {
+            return 1;
+        }
+        int way=0;
+        for(int i=0; i*denoms[choice]<=cents; i++){
+            way+= makeChange(cents-i*denoms[choice], denoms, choice+1);
+        }
+        return way;
+    }
+    /******9.8 represent n cents************************/
 }
