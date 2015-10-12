c87458d5550ba844e3f1da0e9c5df5f9e7517491
wwang <wwanglsu@gmail.com>
2015-10-11 20:18:10 -0700

    test

--- wwang/src/wang/c18/Questions.java
+++ wwang/src/wang/c18/Questions.java
@@ -751,6 +751,10 @@ public class Questions {
 
 
     }
+
+    static void isSquare(){
+
+    }
     /******18.12 N*N matrix with positive and negative, find the submatrix with largest possible sum*************/
 
     /******18. 13 create the largest rectangle of letters such that every row/column forms a word***********/
